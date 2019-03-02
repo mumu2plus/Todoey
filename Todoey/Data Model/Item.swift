e138fd3942798d962df711a490ebfa9830652e8f
mumu2plus <mumu2plus@gmail.com>
2019-03-03 06:22:30 +0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by 加加林 on 2019/3/2.
+//  Copyright © 2019 mumu2plus. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
